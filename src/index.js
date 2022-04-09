73554196bc947b88380b49e5fac87f3cec6d8512
trevorw1990 <95665741+trevorw1990@users.noreply.github.com>
2022-04-09 12:19:11 -0400

    Build

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//Import route and our components
+import { Route, Routes } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default function App () {
+  // We will use the Route component to specify each route
+  return (
+    <div className="App">
+       <Nav />
+    <Routes>
+   
+    </Routes>
+    </div>
+  );
+}
